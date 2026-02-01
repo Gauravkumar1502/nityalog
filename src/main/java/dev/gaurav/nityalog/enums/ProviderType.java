2b6c81c7b5ae618cc27e31db00d11e1491589f57
Gaurav Kumar <gauravkumar15021999@gmail.com>
2026-02-01 22:00:58 +0530

    Refactor ProviderType to include registrationId and update method for OAuth2 provider retrieval

--- src/main/java/dev/gaurav/nityalog/enums/ProviderType.java
+++ src/main/java/dev/gaurav/nityalog/enums/ProviderType.java
@@ -8,14 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public enum ProviderType {
-    EMAIL("Email", 0),
-    GOOGLE("Google", 1),
-    GITHUB("GitHub", 2),
-    FACEBOOK("Facebook", 3),
-    TWITTER("Twitter", 4),
-    DISCORD("Discord", 5),
-    LINKEDIN("Linkedin", 6);
+    EMAIL("email", "Email", 0),
+    GOOGLE("google", "Google", 1),
+    GITHUB("github", "GitHub", 2),
+    FACEBOOK("facebook", "Facebook", 3),
+    TWITTER("twitter", "Twitter", 4),
+    DISCORD("discord", "Discord", 5),
+    LINKEDIN("linkedin", "LinkedIn", 6);
 
+    private final String registrationId;
     private final String displayName;
     private final int code;
 
@@ -28,19 +29,19 @@ public enum ProviderType {
         throw new IllegalArgumentException("Unknown provider code: " + code);
     }
 
-     public ProviderType getProviderTypeFromRegistrationId(String registrationId) {
-        return switch (registrationId) {
-            case "email" -> EMAIL;
-            case "google" -> GOOGLE;
-            case "github" -> GITHUB;
-            case "facebook" -> FACEBOOK;
-            case "twitter" -> TWITTER;
-            case "discord" -> DISCORD;
-            case "linkedin" -> LINKEDIN;
-            default -> {
-                log.error("Unsupported OAuth2 provider: {}", registrationId);
-                throw new IllegalArgumentException("Unsupported OAuth2 provider: " + registrationId);
+    public static ProviderType fromOauth2RegistrationId(String registrationId) {
+        if (registrationId == null) {
+            throw new IllegalArgumentException("OAuth2 registrationId cannot be null");
+        }
+
+        for (ProviderType type : values()) {
+            if (type.getRegistrationId().equalsIgnoreCase(registrationId)) {
+                return type;
             }
-        };
+        }
+
+        log.error("Unsupported OAuth2 provider: {}", registrationId);
+        throw new IllegalArgumentException("Unsupported OAuth2 provider: " + registrationId);
     }
+
 }
