5a6e7918e1a432531ff917ec1820092449406174
Gaurav Kumar <gauravkumar15021999@gmail.com>
2026-02-04 20:33:33 +0530

    Remove unused JSR-310 converters from `AuditableEntity` and `BaseEntity`

--- src/main/java/dev/gaurav/nityalog/entities/BaseEntity.java
+++ src/main/java/dev/gaurav/nityalog/entities/BaseEntity.java
@@ -6,14 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.Instant;
 import java.util.UUID;
 
 @MappedSuperclass
