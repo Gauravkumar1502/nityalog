0197bb35548208b8ec455c47f383231421582c61
Gaurav Kumar <gauravkumar15021999@gmail.com>
2026-02-08 22:56:18 +0530

    remove unused import

--- src/main/java/dev/gaurav/nityalog/entities/Token.java
+++ src/main/java/dev/gaurav/nityalog/entities/Token.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import java.time.Instant;
 import java.util.UUID;
